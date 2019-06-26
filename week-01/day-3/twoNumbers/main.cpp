a590f5299b7eba56cf66fceb02ac19ed2558d976
nemethricsi <nemethricsi@gmail.com>
2019-06-26 15:39:34 +0200

    add type casting to line 19

--- week-01/day-3/twoNumbers/main.cpp
+++ week-01/day-3/twoNumbers/main.cpp
@@ -5,8 +5,6 @@ int main(int argc, char* args[]) {
     // Create a program that prints a few operations on two numbers: 22 and 13
     int a = 22;
     int b = 13;
-    float c = 22.;
-    float d = 13.;
 
     // Print the result of 13 added to 22
     std::cout << "the result of 13 added to 22: " << b + a << std::endl;
@@ -18,7 +16,7 @@ int main(int argc, char* args[]) {
     std::cout << "the result of 22 multiplied by 13: " << b * a << std::endl;
 
     // Print the result of 22 divided by 13 (as a decimal fraction
-    std::cout << "the result of 22 divided by 13 (as a decimal fraction): " << c / d << std::endl;
+    std::cout << "the result of 22 divided by 13 (as a decimal fraction): " << (float)a / (float)b << std::endl;
 
     // Print the integer part of 22 divided by 13
     std::cout << "the integer part of 22 divided by 13: " << a / b << std::endl;
