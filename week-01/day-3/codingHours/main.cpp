448d788f5f415f9c4ead73cf00d17c5fc74bc204
nemethricsi <nemethricsi@gmail.com>
2019-06-26 21:10:26 +0200

    eliminate magic number from line 22

--- week-01/day-3/codingHours/main.cpp
+++ week-01/day-3/codingHours/main.cpp
@@ -14,11 +14,12 @@ int main(int argc, char* args []) {
     int dailyCodingHours = 6;
     int numberOfWeeksInSemester = 17;
     int averageWeeklyWorkingHours = 52;
+    int hoursOfAWeek = 168;
 
 
     std::cout << "An attandee in GFA spends " << numberOfWeeksInSemester * 5 * dailyCodingHours << " hours with coding in a semester if he/she codes only on weekdays." << std::endl;
 
-    std::cout << "That's " << averageWeeklyWorkingHours * 100 / 168 << "% of the whole time in the semester." << std::endl;
+    std::cout << "That's " << averageWeeklyWorkingHours * 100 / hoursOfAWeek << "% of the whole time during the semester." << std::endl;
 
 
     return 0;
