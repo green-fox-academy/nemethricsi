ee6758a5e95bbaad97ffb959b697aa5d299a4010
nemethricsi <nemethricsi@gmail.com>
2019-06-26 14:34:49 +0200

    make exercise HumptyDumpty ready.

---
+++ week-01/day-3/humptyDumpty/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+    std::cout << "Humpty Dumpty had a great fall." << std::endl;
+    std::cout << "All the king's horses and all the king's men" << std::endl;
+    std::cout << "Coulnd't put Humpty together again." << std::endl;
+    return 0;
+}
