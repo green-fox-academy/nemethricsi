f428847c6132faa362b780ea4eb4b34e9a918509
nemethricsi <nemethricsi@gmail.com>
2019-06-27 15:38:36 +0200

    make drawSquare ready

---
+++ week-01/day-4/drawSquare/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%%
+    // %    %
+    // %    %
+    // %    %
+    // %    %
+    // %%%%%%
+    //
+    // The square should have as many lines as the number was
+
+    int userInput;
+    std::cout << "Please enter a number:" << std::endl;
+    std::cin >> userInput;
+
+    std::cout << "%%%%%%" << std::endl;
+
+    for (int i = 0; i < userInput-2; ++i) {
+        std::cout << "%    %" << std::endl;
+    }
+
+    std::cout << "%%%%%%" << std::endl;
+
+    return 0;
+}
