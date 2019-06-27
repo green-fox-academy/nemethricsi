09a83175e1865363eb822b8c1dbac6c66dacb757
nemethricsi <nemethricsi@gmail.com>
2019-06-27 17:10:44 +0200

    eliminate line duplication within the loop

--- week-01/day-4/drawSquare/main.cpp
+++ week-01/day-4/drawSquare/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[]) {
 
     // Write a program that reads a number from the standard input, then draws a
     // square like this:
@@ -16,16 +16,17 @@ int main(int argc, char* args[]) {
     // The square should have as many lines as the number was
 
     int userInput;
-    std::cout << "Please enter a number:" << std::endl;
-    std::cin >> userInput;
+    std::cout << "Please enter a number:" << std::endl;
+    std::cin >> userInput;
 
-    std::cout << "%%%%%%" << std::endl;
 
-    for (int i = 0; i < userInput-2; ++i) {
-        std::cout << "%    %" << std::endl;
+    for (int i = 0; i <= userInput; ++i) {
+        if (i == 0 || i == userInput) {
+            std::cout << "%%%%%%" << std::endl;
+        } else {
+            std::cout << "%    %" << std::endl;
+        }
     }
-
-    std::cout << "%%%%%%" << std::endl;
 
     return 0;
 }
