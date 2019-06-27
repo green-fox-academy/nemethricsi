a203aaa3baef1d52b219dd13535a2743760aea4a
nemethricsi <nemethricsi@gmail.com>
2019-06-27 20:56:57 +0200

    minor changes

--- week-01/day-4/drawDiamond/main.cpp
+++ week-01/day-4/drawDiamond/main.cpp
@@ -18,8 +18,24 @@ int main(int argc, char *args[]) {
     int userInput;
     std::cout << "Please enter a number:" << std::endl;
     std::cin >> userInput;
-    int n = userInput / 2;
+    int halfDiamond = userInput / 2;
 
+    for (int i = 0; i < userInput; ++i) {
+        if (i < (userInput + 1) / 2) {
+            for (int j = 0; j <= userInput - i - 1; ++j) {
+                std::cout << " ";
+            }
+            for (int k = 0; k < userInput + i - (userInput - i - 1); k++) {
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        } else if () {
+
+        }
+    }
+
+
+    /****************************************
     for (int i = 0; i < userInput; ++i) {
         if (i < (userInput + 1) / 2) {
             for (int j = 0; j <= n - i - 4; ++j) {
@@ -39,6 +55,23 @@ int main(int argc, char *args[]) {
             std::cout << std::endl;
         }
     }
+    *****************************************/
 
     return 0;
 }
+
+/****************************************
+PYRAMID
+
+
+for (int i = 0; i <= userInput-1 ; ++i) {
+    for (int j = 0; j <= userInput - i - 1; ++j) {
+        std::cout << " ";
+    }
+    for (int k = 0; k < userInput + i - (userInput - i - 1); k++) {
+        std::cout << "*";
+    }
+    std::cout << std::endl;
+}
+
+*****************************************/
