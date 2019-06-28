e5aabfad4f7ff8cb20005c9da0597482c43ef3ce
nemethricsi <nemethricsi@gmail.com>
2019-06-28 15:58:35 +0200

    make exercise third ready

---
+++ week-01/day-5/third.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+int main(int argc, char *args[])
+{
+
+    int q[] = {4, 5, 6, 7};
+
+    std::cout << q[2] << std::endl;
+
+    return 0;
+}
+
