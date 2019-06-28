78693954bc27df42089452ff28fe2686b6c9ef59
nemethricsi <nemethricsi@gmail.com>
2019-06-28 16:13:52 +0200

    make exercise compare length ready

---
+++ week-01/day-5/compare_length.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print "p2 is longer" if `p2` has more elements than `p1`
+
+int main(int argc, char* args[]) {
+
+    int p1 [] = {1, 2, 3};
+    int p2 [] = {4, 5};
+
+    if(sizeof(p1)/ sizeof(p1[0]) > sizeof(p2)/ sizeof(p2[0])){
+        std::cout << "p1 is longer" << std::endl;
+    } else if(sizeof(p1)/ sizeof(p1[0]) < sizeof(p2)/ sizeof(p2[0])){
+        std::cout << "p2 is longer" << std::endl;
+    } else {
+        std::cout << "p2 and p1 have the same number of elements." << std::endl;
+    }
+    
+    return 0;
+}
