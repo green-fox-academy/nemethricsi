f3962e0c63d044c587057cbf1f8ba2a7ad473d07
nemethricsi <nemethricsi@gmail.com>
2019-06-30 13:54:07 +0200

    fix the style

--- week-01/day-5/matrix.cpp
+++ week-01/day-5/matrix.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
 #include <string>
 
-// - Create (dynamically) a two dimensional array
-//   with the following matrix. Use a loop!
-//   by dynamically, we mean here that you can change the size of the matrix
-//   by changing an input value or a parameter or this combined
-//
-//   1 0 0 0
-//   0 1 0 0
-//   0 0 1 0
-//   0 0 0 1
-//
-// - Print this two dimensional array to the output
-
 int main(int argc, char *args[])
 {
-
-    int n = 0; // for a matrix n x n
+    int n = 0;
     std::cout << "Enter a number (n) for drawing a n*n matrix: ";
     std::cin >> n;
 
     int matrix[n][n];
 
-    for (int i = 0; i <n; ++i) {
-        for (int j = 0; j <n; ++j) {
-            if (i == j){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == j) {
                 matrix[i][j] = 1;
             } else {
                 matrix[i][j] = 0;
             }
         }
-
     }
 
     for (int k = 0; k < n; ++k) {
