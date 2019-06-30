aaa381e6f6dee9fc01f23e98a28b8e6434050b42
nemethricsi <nemethricsi@gmail.com>
2019-06-30 18:23:08 +0200

    fix the style

--- week-01/day-5/unique.cpp
+++ week-01/day-5/unique.cpp
@@ -20,19 +20,19 @@ int main(int argc, char *args[])
 
 int unique(int arr[], int n)
 {
-    if (n == 0 || n == 1){
+    if (n == 0 || n == 1) {
         return n;
     }
 
     int tempArray[n];
     int j = 0;
 
-    for (int i = 0; i < n-1; ++i) {
-        if(arr[i] != arr[i + 1]){
+    for (int i = 0; i < n - 1; ++i) {
+        if (arr[i] != arr[i + 1]) {
             tempArray[j++] = arr[i];
         }
     }
-    tempArray[j++] = arr[n-1];
+    tempArray[j++] = arr[n - 1];
 
     for (int i = 0; i < j; i++) {
         arr[i] = tempArray[i];
