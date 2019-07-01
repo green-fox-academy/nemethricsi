d106e883179485dae8cf9f26c81a8d935e769b2a
nemethricsi <nemethricsi@gmail.com>
2019-07-01 14:20:36 +0200

    add exercise Todo Print

---
+++ week-02/day-1/todo_print.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+// Add "My todo:" to the beginning of the todoText
+// Add " - Download games" to the end of the todoText
+// Add " - Diablo" to the end of the todoText but with indentation
+
+// Expected outpt:
+
+// My todo:
+//  - Buy milk
+//  - Download games
+//      - Diablo
+
+int main(int argc, char *args[])
+{
+    std::string todoText = " - Buy milk\n";
+
+    todoText.insert(0, "My todo:\n");
+
+    todoText.append(" - Download games\n");
+    todoText.append("    ");
+    todoText.append(" - Download games\n");
+
+    std::cout << todoText << std::endl;
+
+    return 0;
+}
