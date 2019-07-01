24644ede5a98a716e328dcb5ef8765e7b1e28c67
nemethricsi <nemethricsi@gmail.com>
2019-07-01 15:48:31 +0200

    simplify Bubble sort function

--- week-01/day-5/bubble_sort.cpp
+++ week-01/day-5/bubble_sort.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 
-void bubbleSort(int arr[], int n, bool x);
+void bubbleSort(int arr[], int n, bool desc);
 
 void printArray(int arr[], int n);
 
@@ -23,25 +23,9 @@ int main(int argc, char *args[])
     return 0;
 }
 
-void bubbleSort(int arr[], int n, bool x)
+void bubbleSort(int arr[], int n, bool desc)
 {
-    if(x == false){
-        int temp = 0;
-        for (int j = 0; j < n; ++j) {
-            for (int i = 0; i < n - 1; ++i) {
-                if (arr[i] > arr[i + 1]) {
-                    temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
-                }
-            }
-            if (arr[n - 2] > arr[n - 1]) {
-                temp = arr[n - 2];
-                arr[n - 2] = arr[n - 1];
-                arr[n - 1] = temp;
-            }
-        }
-    } else {
+    if (desc) {
         int temp = 0;
         for (int j = 0; j < n; ++j) {
             for (int i = n - 1; i > 0; --i) {
@@ -51,14 +35,19 @@ void bubbleSort(int arr[], int n, bool x)
                     arr[i - 1] = temp;
                 }
             }
-            if (arr[1] > arr[0]) {
-                temp = arr[1];
-                arr[1] = arr[0];
-                arr[0] = temp;
+        }
+    } else {
+        int temp = 0;
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < n - 1; ++i) {
+                if (arr[i] > arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
             }
         }
     }
-
 }
 
 void printArray(int arr[], int n)
