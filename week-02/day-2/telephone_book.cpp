351b6cd6a0e781cbc9ce210d22071324edfdc454
nemethricsi <nemethricsi@gmail.com>
2019-07-04 15:43:34 +0200

    initial commit

---
+++ week-02/day-2/telephone_book.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+
+
+int main(int argc, char *args[])
+{
+    std::map<std::string, std::string> phonebook = {
+            {"William A. Lathan", "405-709-1865"},
+            {"John K. Miller", "402-247-8568"},
+            {"Hortensia E. Foster", "606-481-6467"},
+            {"Amanda D. Newland", "319-243-5613"},
+            {"Brooke P. Askew", "307-687-2982"}
+    };
+
+    return 0;
+}
