4503c186b20dfb2355b9bcd5af11d3787a41a5c3
nemethricsi <nemethricsi@gmail.com>
2019-07-04 15:35:02 +0200

    update CMakeList

--- week-02/day-4/envelope_star.cpp
+++ week-02/day-4/envelope_star.cpp
@@ -6,7 +6,7 @@ void draw(SDL_Renderer *gRenderer)
 {
     int fullSize = 400;
     int halfSize = fullSize / 2;
-    int step = 10; // decrease until 1 to make it thicker
+    int step = 6; // decrease until 1 to make it thicker
     SDL_SetRenderDrawColor(gRenderer, 0x3B, 0xB8, 0x78, 0xFF);
 
 
