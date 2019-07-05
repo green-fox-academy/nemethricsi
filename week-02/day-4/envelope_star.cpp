1bb60f8f407ee24684a4d812f723de1b84cc1e3d
nemethricsi <nemethricsi@gmail.com>
2019-07-05 16:24:50 +0200

    make envelope star exercise shorter

--- week-02/day-4/envelope_star.cpp
+++ week-02/day-4/envelope_star.cpp
@@ -1,32 +1,21 @@
 #include "draw.h"
 
-// Green Fox green Color: #3BB878 lila: #6F73D2
-
 void draw(SDL_Renderer *gRenderer)
 {
     int fullSize = 400;
     int halfSize = fullSize / 2;
-    int step = 6; // decrease until 1 to make it thicker
-    SDL_SetRenderDrawColor(gRenderer, 0x3B, 0xB8, 0x78, 0xFF);
+    int step = 10; // decrease until 1 to make it thicker
 
 
-    // TOP - RIGHT
+    SDL_SetRenderDrawColor(gRenderer, 0x94, 0x3D, 0x77, 0xFF);
     for (int i = 0; i <= halfSize; i += step) {
+        // TOP - RIGHT
         SDL_RenderDrawLine(gRenderer, halfSize, i, i + halfSize, halfSize);
-    }
-
-    // BOTTOM - LEFT
-    for (int i = 0; i <= halfSize; i += step) {
+        // BOTTOM - LEFT
         SDL_RenderDrawLine(gRenderer, i, halfSize, halfSize, i + halfSize);
-    }
-
-    // TOP - LEFT
-    for (int i = 0; i <= halfSize; i += step) {
+        // TOP - LEFT
         SDL_RenderDrawLine(gRenderer, i, halfSize, halfSize, halfSize - i);
-    }
-
-    // BOTTOM - RIGHT
-    for (int i = 0; i <= halfSize; i += step) {
-        SDL_RenderDrawLine(gRenderer, 2 * halfSize - i, halfSize, halfSize, i + halfSize);
+        // BOTTOM - RIGHT
+        SDL_RenderDrawLine(gRenderer, fullSize - i, halfSize, halfSize, i + halfSize);
     }
 }
