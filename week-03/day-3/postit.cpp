1ef15f5818796a82055c5fb9569e0f8ea9dadbea
nemethricsi <nemethricsi@gmail.com>
2019-07-10 14:34:46 +0200

    add exercise Post-it

---
+++ week-03/day-3/postit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "PostItClass.h"
+
+int main()
+{
+
+    PostItClass orangePostIt("orange", "Idea 1", "blue");
+    PostItClass pinkPostIt("pink", "Awesome!", "black");
+    PostItClass yellowPostTt("orange", "Superb!", "yellow");
+
+
+    return 0;
+}
