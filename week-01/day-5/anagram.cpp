d218b50b242c57f67dbb8adbf8ca162c2a50c3e4
Richárd Németh <nemethricsi@gmail.com>
2019-07-17 17:41:49 +0200

    add Anagram exercise

---
+++ week-01/day-5/anagram.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+std::map<char, int> stringToMap(std::string w);
+
+bool areTheyAnagrams(std::string word1, std::string word2);
+
+int main(int argc, char *args[])
+{
+    std::cout << areTheyAnagrams("alma", "aalm") << std::endl;
+
+    return 0;
+}
+
+std::map<char, int> stringToMap(std::string word){
+    std::map<char, int> mapWord;
+    for (int i = 0; i < word.size(); ++i) {
+        if(mapWord.find(word.at(i)) != mapWord.end()){
+            mapWord.insert(std::make_pair(word.at(i), 1));
+        } else {
+            mapWord[word.at(i)] += 1;
+        }
+    }
+    return mapWord;
+}
+
+bool areTheyAnagrams(std::string word1, std::string word2)
+{
+    if(word1.size() != word2.size()) {
+        return false;
+    }
+
+    std::map<char, int> map1 = stringToMap(word1);
+    std::map<char, int> map2 = stringToMap(word2);
+
+    if(map1.size() != map2.size()){
+        return false;
+    }
+
+    std::map<char, int>::iterator it;
+    for (it = map1.begin(); it != map1.end() ; ++it) {
+        if(!(map2.find(it->first) != map2.end() && it->second == map2[it->first])){
+            return false;
+        }
+    }
+    return true;
+}
