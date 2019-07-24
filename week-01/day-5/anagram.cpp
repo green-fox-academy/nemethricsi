83e3d3b81a9722553f25570337a347f16822ee2e
Richárd Németh <nemethricsi@gmail.com>
2019-07-24 07:42:00 +0200

    fix Anagram exercise

--- week-01/day-5/anagram.cpp
+++ week-01/day-5/anagram.cpp
@@ -2,21 +2,28 @@
 #include <string>
 #include <map>
 
-std::map<char, int> stringToMap(std::string w);
+std::map<char, int> stringToMap(std::string word);
 
 bool areTheyAnagrams(std::string word1, std::string word2);
 
 int main(int argc, char *args[])
 {
-    std::cout << areTheyAnagrams("alma", "aalm") << std::endl;
+    std::string userInput;
+    std::string userInput2;
+    std::cout << "Type two words: " << std::endl;
+    std::cin >> userInput >> userInput2;
+    std::cout << "\"" << userInput << "\"" << " and " << "\"" << userInput2 << "\"" << " are Anagrams: "
+              << std::boolalpha
+              << areTheyAnagrams(userInput, userInput2) << std::endl;
 
     return 0;
 }
 
-std::map<char, int> stringToMap(std::string word){
+std::map<char, int> stringToMap(std::string word)
+{
     std::map<char, int> mapWord;
     for (int i = 0; i < word.size(); ++i) {
-        if(mapWord.find(word.at(i)) != mapWord.end()){
+        if (mapWord.find(word.at(i)) == mapWord.end()) {
             mapWord.insert(std::make_pair(word.at(i), 1));
         } else {
             mapWord[word.at(i)] += 1;
@@ -27,20 +34,20 @@ std::map<char, int> stringToMap(std::string word){
 
 bool areTheyAnagrams(std::string word1, std::string word2)
 {
-    if(word1.size() != word2.size()) {
+    if (word1.size() != word2.size()) {
         return false;
     }
 
     std::map<char, int> map1 = stringToMap(word1);
     std::map<char, int> map2 = stringToMap(word2);
 
-    if(map1.size() != map2.size()){
+    if (map1.size() != map2.size()) {
         return false;
     }
 
     std::map<char, int>::iterator it;
-    for (it = map1.begin(); it != map1.end() ; ++it) {
-        if(!(map2.find(it->first) != map2.end() && it->second == map2[it->first])){
+    for (it = map1.begin(); it != map1.end(); ++it) {
+        if (!(map2.find(it->first) != map2.end() && it->second == map2[it->first])) {
             return false;
         }
     }
