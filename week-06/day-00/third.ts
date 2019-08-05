0a7ccb47d4ea20d860912311a86558b3ba5364c5
Richard Nemeth <nemethricsi@gmail.com>
2019-08-05 23:17:52 +0200

    add Third exercise

---
+++ week-06/day-00/third.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
+
+let magicNumbers = [1, 3, 5, 7];
+console.log(magicNumbers[2]);
