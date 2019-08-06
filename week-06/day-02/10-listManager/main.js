36c5e5c1c17e7a3d99674fd2282dee1484f20c7b
Richard Nemeth <nemethricsi@gmail.com>
2019-08-06 18:09:32 +0200

    add List Manager - not ready yet

---
+++ week-06/day-02/10-listManager/main.js
@@ -0,0 +1,17 @@
+// 1, Build up the necessary structure with JavaScript (try to use semantically correct elements)
+//     2, Achive the same design by css
+//     3, Try not to use css classes
+//     4, Add some functionality to the buttons
+//         - If "Up" is clicked the selection should move up by one
+//         - If "Down" is clicked the selection should move down by one
+//         - If "X" is clicked the selected item should be removed and the first item should be selected
+//         - If ">" is clicked the selected item should be moved to the right side and the first item on the left side should be selected
+//     5, Check all the edge cases, no error should be printed to the console
+
+let shoppingList = ['bread', 'milk', 'orange', 'tomato'];
+
+// for (let i = 0; i < shoppingList.length; i++) {
+//   let newItem = document.createElement('h1');
+//   newItem.textContent = shoppingList[i];
+//   document.querySelector('.left').appendChild(newItem);
+// }
