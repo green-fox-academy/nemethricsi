b6454e21584efbb5151485bedecdf00aa6fc825d
Richard Nemeth <nemethricsi@gmail.com>
2019-08-06 11:59:31 +0200

    add queerySelector exercise

---
+++ week-06/day-02/querySelector/queryselector-1.js
@@ -0,0 +1,25 @@
+// 1. store the element that says 'The King' in the 'king' variable.
+//       console.log it.
+let king = document.querySelector('#b325');
+console.log(king);
+
+// 2. store 'The Businessman' and 'The Lamplighter'
+//       in the 'businessLamp' variable.
+//       console.log each of them.
+let businessLamp = document.querySelectorAll('.big');
+console.log(businessLamp);
+
+// 3. store 'The King' and 'The Conceited Man'
+//       in the 'conceitedKing' variable.
+//       alert them one by one.
+let conceitedKing = document.querySelectorAll('.container > .asteroid');
+console.log(conceitedKing);
+
+// 4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+//       in the 'noBusiness' variable.
+//       console.log each of them.
+let noBusiness = document.querySelectorAll('div.asteroid');
+for (let i = 0; i < conceitedKing2.length; i++) {
+  console.log(conceitedKing2[i]);
+}
+console.log(conceitedKing2.length);
