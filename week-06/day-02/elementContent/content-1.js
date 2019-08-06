256fccf83efdfa329fba499c105ed43f49557b1f
Richard Nemeth <nemethricsi@gmail.com>
2019-08-06 12:19:29 +0200

    add elementContent exercise

---
+++ week-06/day-02/elementContent/content-1.js
@@ -0,0 +1,12 @@
+// 2)  Do the same again, but you should keep the cat strong.
+let everyP2 = document.querySelectorAll('p');
+for (let i = 0; i < everyP2.length; i++) {
+  everyP2[i].innerHTML = everyP2[everyP2.length - 1].innerHTML;
+}
+
+// 1)  Fill every paragraph with the last one's content.
+
+let everyP = document.querySelectorAll('p');
+for (let i = 0; i < everyP.length; i++) {
+  everyP[i].textContent = everyP[everyP.length - 1].textContent;
+}
