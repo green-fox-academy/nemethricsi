dedffd60a8f08329096f2e683d632f260a51342c
Richard Nemeth <nemethricsi@gmail.com>
2019-08-06 16:27:41 +0200

    add Remove Element exercise

---
+++ week-06/day-02/07-removeElement/createelement-2.js
@@ -0,0 +1,48 @@
+const planetData = [
+  {
+    category: 'inhabited',
+    content: 'Foxes',
+    asteroid: true
+  },
+  {
+    category: 'inhabited',
+    content: 'Whales and Rabbits',
+    asteroid: true
+  },
+  {
+    category: 'uninhabited',
+    content: 'Baobabs and Roses',
+    asteroid: true
+  },
+  {
+    category: 'inhabited',
+    content: 'Giant monsters',
+    asteroid: false
+  },
+  {
+    category: 'inhabited',
+    content: 'Sheep',
+    asteroid: true
+  }
+];
+
+// Remove the king from the list.
+document.querySelector('.asteroids').removeChild(document.querySelector('li'));
+
+// Fill the list based on the following list of objects.
+// Only add the asteroids to the list.
+// Each list item should have its category as a class and its content as text content.
+
+let newList = [];
+
+for (let i = 0; i < planetData.length; i++) {
+  if (planetData[i].asteroid === true) {
+    newList.push(planetData[i].content);
+  }
+}
+
+for (let i = 0; i < newList.length; i++) {
+  let newItem = document.createElement('li');
+  newItem.innerHTML = newList[i];
+  document.querySelector('.asteroids').appendChild(newItem);
+}
