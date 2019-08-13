0914a44f53266f329d1d0758e0a7ad3f41ff5979
Richard Nemeth <nemethricsi@gmail.com>
2019-08-13 17:39:36 +0200

    update Giphy exercise w Flexbox

--- week-07/day-02/XHR/Giphy-API/main.js
+++ week-07/day-02/XHR/Giphy-API/main.js
@@ -6,16 +6,17 @@ const xhr = new XMLHttpRequest();
 xhr.onload = function() {
   let myData = JSON.parse(xhr.responseText);
   console.log(myData);
-  console.log(myData.data[0].images['480w_still'].url);
 
-  let imgList = document.querySelectorAll('img');
+  let imgList = document.querySelectorAll('.cell');
   for (let i = 0; i < imgList.length; i++) {
-    // imgList[i].src = `https://media.giphy.com/media/${
-    //   myData.data[i].id
-    // }/giphy.gif`;
-    imgList[i].src = myData.data[i].images['480w_still'].url;
+    imgList[i].style.backgroundImage = `url(${
+      myData.data[i].images['480w_still'].url
+    })`;
     imgList[i].onclick = function() {
-      imgList[i].src = myData.data[i].images['downsized'].url;
+      imgList[i].style.backgroundImage = `url(${
+        myData.data[i].images['downsized'].url
+      })`;
+      cursor;
     };
   }
 };
