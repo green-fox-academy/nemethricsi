408575eacabe7333a39ca2448b40c3454e5808f6
Richard Nemeth <nemethricsi@gmail.com>
2019-08-14 14:58:51 +0200

    add Doubling exercise

--- week-07/day-03/Frontend/app.js
+++ week-07/day-03/Frontend/app.js
@@ -8,6 +8,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+// Create a GET /doubling endpoint
+// It should be able to handle a query parameter called input, eg. http://localhost:8080/doubling?input=15
+// This endpoint should respond with a json object with the doubled amount of the input:
+
+app.get('/doubling', (req, res) => {
+  if (Number(req.query.input) === NaN) {
+    const response = {
+      error: 'Please provide an input!'
+    };
+  }
+  const response = {
+    received: req.query.input,
+    result: Number(req.query.input) * 2
+  };
+  res.send(response);
+});
+
 app.listen(PORT, () =>
   console.log(`The server is up and running on PORT ${PORT}`)
 );
