f708315b229e9bbeeb29902ec23054c58754d64a
Richard Nemeth <nemethricsi@gmail.com>
2019-08-24 19:33:34 +0200

    update Reddit exercise

--- week-07/day-05/Reddit/server.js
+++ week-07/day-05/Reddit/server.js
@@ -10,6 +10,8 @@ const connection = mysql.createConnection({
   database: 'reddit'
 });
 
+app.use(express.static('public'))
+
 connection.connect(function (err) {
   if (err) {
     console.log('Error connecting to Db');
@@ -18,18 +20,41 @@ connection.connect(function (err) {
   console.log('Connection established');
 });
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
 
 app.get('/posts', (req, res) => {
-  connection.query('SELECT title, url FROM posts;', (err, rows) => {
+  connection.query('SELECT *, url FROM posts;', (err, rows) => {
     res.send({ posts: rows });
   });
 });
 
-// app.post('/posts', (req, res) => {
-// });
+app.post('/posts', (req, res) => {
+  const query = 'INSERT INTO posts(title, url) VALUES(?, ?);';
+  connection.query(query, [req.body.title, req.body.url], (err, result) => {
+    if (err === null) {
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(500);
+    }
+  });
+});
+
+app.post('/account', (req, res) => {
+  const { result } = req.body;
+  const query = `insert into users(username,password) values(?, ?);`;
+  console.log(req.body);
+  connection.query(query, [req.body.username, req.body.password], (err, result) => {
+    if (err === null) {
+      res.sendStatus(201);
+    }
+    else {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => { console.log(`Server is up and listening on port ${PORT}`); })
