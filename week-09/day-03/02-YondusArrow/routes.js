e83bd00b777006cf91470611235435250d25d743
Richard Nemeth <nemethricsi@gmail.com>
2019-09-05 00:11:22 +0200

    add test

--- week-09/day-03/02-YondusArrow/routes.js
+++ week-09/day-03/02-YondusArrow/routes.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/yondu', (req, res) => {
-    // implement your METHOD here
     const distance = req.query.distance;
     const time = req.query.time;
-    res.send({
-        distance: Number(distance).toFixed(1),
-        time: Number(time).toFixed(1),
-        speed: (distance / time).toFixed(1)
-    });
+    if (time > 0) {
+        res.send({
+            distance: Number(distance).toFixed(1),
+            time: Number(time).toFixed(1),
+            speed: (distance / time).toFixed(1)
+        });
+    } else {
+        res.status(400).send({ error: 'time have to be bigger than 0' });
+    }
+
 });
 
 module.exports = app;
--- week-09/day-03/02-YondusArrow/test/test-routes.js
+++ week-09/day-03/02-YondusArrow/test/test-routes.js
@@ -4,6 +4,25 @@ const test = require('tape');
 const request = require('supertest');
 const app = require('../routes');
 
-test('test name', (t) => {
+test('speed-o-meter w/ given parameter is ok', (t) => {
     // implement your TEST here
+    request(app)
+        .get('/yondu?distance=100.0&time=10.0')
+        .send()
+        .end((err, res) => {
+            if (err) throw err;
+
+            t.equal(res.status, 200);
+            t.same(res.body, {
+                distance: "100.0",
+                time: "10.0",
+                speed: "10.0"
+            });
+            t.end();
+        });
+});
+
+test('speed-o-meter w/o given parameter is not ok', (t) => {
+    request(app)
+        .get('/yondu/yondu?distance=&time=')
 });
