74a596e065dde71a9db271b5ed5726bdd9000c20
Richard Nemeth <nemethricsi@gmail.com>
2019-09-06 13:11:42 +0200

    show posts from database on starting screen

--- week-09/day-04-05/Reddit-2/app/routes.js
+++ week-09/day-04-05/Reddit-2/app/routes.js
@@ -1,34 +1,42 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const fetch = require('node-fetch');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'reddit'
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'reddit'
 });
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('./public'))
 app.use(express.json()); //enable to parse req.body !
 
 connection.connect(function (err) {
-    if (err) {
-        console.log('Error connecting to Db');
-        return;
-    }
-    console.log('Connection established');
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
 });
 
 app.get('/', (req, res) => {
-    // implement your METHOD here
-    res.render('index');
+  // implement your METHOD here
+  res.render('index');
 });
 
+
 app.get('/posts', (req, res) => {
-    connection.query('SELECT * FROM posts;', (err, rows) => {
-        res.send({ posts: rows });
-    });
+  connection.query('SELECT * FROM posts ORDER BY id DESC;', (err, rows) => {
+    res.send({ posts: rows });
+  });
+});
+
+app.get('/api/posts', (req, res) => {
+  fetch('http://localhost:3000/posts')
+    .then(answer => answer.json())
+    .then(json => res.render('posts', { posts: json }));
 });
 
 module.exports = app;
--- week-09/day-04-05/Reddit-2/public/js/main.js
+++ week-09/day-04-05/Reddit-2/public/js/main.js
@@ -1,1 +1,7 @@
 const mainContent = document.querySelector('main');
+
+mainContent.innerHTML = '<h1>That works</h1>';
+
+fetch('/api/posts')
+  .then(res => res.text())
+  .then(posts => mainContent.innerHTML = posts);
