a3d2f8c1a13516e68cc32709005741f19077a72e
Richard Nemeth <nemethricsi@gmail.com>
2019-09-06 10:58:43 +0200

    init mysql in node

--- week-09/day-04-05/Reddit-2/app/routes.js
+++ week-09/day-04-05/Reddit-2/app/routes.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'reddit'
+});
 
 app.get('/', (req, res) => {
     // implement your METHOD here
--- week-09/day-04-05/Reddit-2/server.js
+++ week-09/day-04-05/Reddit-2/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const routes = require('./routes');
+const routes = require('./app/routes');
 const PORT = 3000;
 
 routes.listen(PORT, () => {
