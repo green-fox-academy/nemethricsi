cf6d3902bf23747e7a02ff15d14d8fc19478f856
Richard Nemeth <nemethricsi@gmail.com>
2019-09-09 19:22:10 +0200

    add helping span text to icons

--- week-10/FoxPlayer/assets/js/main.js
+++ week-10/FoxPlayer/assets/js/main.js
@@ -1,6 +1,7 @@
 const tracks = document.querySelectorAll('.track-item');
 tracks.forEach(element => {
   element.addEventListener('click', e => {
+    //play that title / track
     console.log(e.target.childNodes[1].textContent);
   });
 });
@@ -17,12 +18,11 @@ const customPlaylists = document.querySelectorAll('.custom');
 customPlaylists.forEach(element => {
   element.addEventListener('click', e => {
     // list that playlist in tracklist
-    console.log('megnyitja a playlistet');
     console.log(e.target.childNodes[1].textContent);
   });
 });
 
-const closePlaylist = document.querySelector('.fa-times');
-closePlaylist.addEventListener('click', () => {
-  console.log('ez majd bezárja a playlistet');
+const deletePlaylist = document.querySelector('.fa-times');
+deletePlaylist.addEventListener('click', () => {
+  console.log('this should delete the playlist');
 });
