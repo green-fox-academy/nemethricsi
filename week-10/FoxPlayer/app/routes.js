0704bf6f93da31879a7a6ca7d2cd596a1487a878
Richard Nemeth <nemethricsi@gmail.com>
2019-09-10 12:51:26 +0200

    connect MYSQL to app

--- week-10/FoxPlayer/app/routes.js
+++ week-10/FoxPlayer/app/routes.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'foxplayer'
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('./assets'));
 // app.use(express.json()); //enable to parse req.body !
+
+connection.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to DB');
+    return;
+  }
+  console.log('Connection established with DB');
+});
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/playlists', (req, res) => {
-  // lists all the playlists
+  connection.query('SELECT * FROM playlists;', (err, rows) => {
+    res.send(rows);
+  });
 });
 
 app.get('/playlist-tracks', (req, res) => {
-  //  it should return all tracks in the root folder
-  // Subfolders should be explored recursively.
+  connection.query('SELECT * FROM tracks;', (err, rows) => {
+    res.send(rows);
+  });
+});
+
+app.get('/playlist-tracks/:playlist_id', (req, res) => {
+  connection.query(`SELECT * FROM tracks WHERE playlist_id = ${req.params.playlist_id};`, (err, rows) => {
+    res.send(rows);
+  });
+});
+
+app.get('/custom-playlists', (req, res) => {
+  connection.query(`SELECT * FROM playlists WHERE playlist_id != 1;`, (err, rows) => {
+    res.send(rows);
+  });
 });
 
 module.exports = app;
